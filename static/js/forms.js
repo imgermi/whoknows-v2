c894939269b2ce3f50900bf999c39420245cecdc
imgermi <juanmartingermano@gmail.com>
2020-04-05 19:58:41 -0300

    form testing

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,8 +1,8 @@
-$("#contactForm").submit(function(e) {
-  e.preventDefault();
+// $("#contactForm").submit(function(e) {
+//   e.preventDefault();
 
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    $("#contactForm").append("Some appended text."); 
-  });
-});
+//   var $form = $(this);
+//   $.post($form.attr("action"), $form.serialize()).then(function() {
+//     alert("Thank youuuuu!");
+//   });
+// });
